7d5650fef1d29d74a924103de85da2d31c1a348e
MaliakaMadi <madimaliaka@gmail.com>
2026-02-17 16:00:14 +0000

    Add `main.js` for client-side features, refactor translation handling, and update setup documentation.

---
+++ backend/src/api/disease-surveillance/controllers/disease-surveillance.ts
@@ -0,0 +1,7 @@
+/**
+ * disease-surveillance controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::disease-surveillance.disease-surveillance');
---
+++ backend/src/api/disease-surveillance/routes/disease-surveillance.ts
@@ -0,0 +1,7 @@
+/**
+ * disease-surveillance router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::disease-surveillance.disease-surveillance');
